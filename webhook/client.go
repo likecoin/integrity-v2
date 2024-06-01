00e2a51fe32147d9487aeb035a907154c4871382
William Chong <me@williamchong.cloud>
2024-06-02 00:52:25 +0800

    [webhook] support cbor as metadata format

--- webhook/client.go
+++ webhook/client.go
@@ -7,8 +7,11 @@ import (
 	"io"
 	"mime/multipart"
 	"net/http"
+	"net/textproto"
 	urlpkg "net/url"
+	"strings"
 
+	"github.com/fxamacker/cbor/v2"
 	"github.com/starlinglab/integrity-v2/config"
 )
 
@@ -18,11 +21,22 @@ var client = &http.Client{}
 type PostGenericWebhookOpt struct {
 	Source    string // Source is the origin of the asset, which is used to determine the webhook endpoint
 	ProjectId string // ProjectId is the project specific ID where the asset belongs
+	Format    string // Format is "json" or "cbor"
 }
 
 type PostGenericWebhookResponse struct {
 	Cid   string `json:"cid,omitempty"`
 	Error error  `json:"error,omitempty"`
+}
+
+// createFormFieldWithContentType is a modified multipart.Writer.CreateFormField
+// which allow setting of content-type
+func createFormFieldWithContentType(w *multipart.Writer, fieldName string, contentType string) (io.Writer, error) {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"`, strings.NewReplacer("\\", "\\\\", `"`, "\\\"").Replace(fieldName)))
+	h.Set("Content-Type", contentType)
+	return w.CreatePart(h)
 }
 
 // PostFileToWebHook posts a file and its metadata to the webhook server
@@ -42,23 +56,33 @@ func PostFileToWebHook(file io.Reader, metadata map[string]any, opts PostGeneric
 	pr, pw := io.Pipe()
 	mp := multipart.NewWriter(pw)
 
+	metadataFormatType := "application/json"
+	if opts.Format == "cbor" {
+		metadataFormatType = "application/cbor"
+	}
+
 	go func() {
-		metadataString, err := json.Marshal(metadata)
+		metadataPart, err := createFormFieldWithContentType(mp, "metadata", metadataFormatType)
 		if err != nil {
 			pw.CloseWithError(err)
 			return
 		}
-		err = mp.WriteField("metadata", string(metadataString))
+		if metadataFormatType == "application/cbor" {
+			err = cbor.NewEncoder(metadataPart).Encode(metadata)
+
+		} else {
+			err = json.NewEncoder(metadataPart).Encode(metadata)
+		}
 		if err != nil {
 			pw.CloseWithError(err)
 			return
 		}
-		part, err := mp.CreateFormFile("file", "")
+		filePart, err := mp.CreateFormFile("file", "")
 		if err != nil {
 			pw.CloseWithError(err)
 			return
 		}
-		_, err = io.Copy(part, file)
+		_, err = io.Copy(filePart, file)
 		if err != nil {
 			pw.CloseWithError(err)
 			return
